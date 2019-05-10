f8c34fb9c4fc1c30ea95456fbcf9967ebf73b826
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-05-10 06:52:52 -0300

    Update menu.pl

--- menu.pl
+++ menu.pl
@@ -1,20 +1,68 @@
-raca(1) :- write('tamanduá.'), nl.
-raca(2) :- write('tucano.'), nl.
-raca(3) :- write('lobo-guara.'), nl.
-raca(4) :- write('gato.'), nl. 
-raca(5) :- write('cachorro.'), nl.
-raca(6) :- write('papagaio.'), nl.
-raca(7) :- write('frango.'), nl.
-raca(8) :- write('jabuti.'), nl.
-raca(9) :- write('tatu.'), nl.
+raca(1) :- write('tamanduá-bandeira').
+raca(2) :- write('tucano').
+raca(3) :- write('lobo-guara').
 
-raca_seler :- X is random(10), raca(X), nl.
+especie(1) :- 'tamanduá-bandeira'.
+especie(2) :- 'tucano'.
+especie(3) :- 'lobo-guara'.
 
-clear :- write('\33\[2J').
 
-opt(1) :- nl, write('Tamagotchi tá nascendoo...'), nl, nl, sleep(2), write('Parabéns! Vc acaba de ter um '),raca_seler, nl.
 
-opt(2) :- nl, write('Tamagotchi se foi...'), nl.
+print_raca(X):- X = raca(X),write(X).
+
+
+:- dynamic tamagotchi/2.
+
+tamagotchi(nome, especie).
+
+:- dynamic nroSobre/0,nome/0, tipo/0.
+roSobre.
+Nome.
+Tipo.
+
+
+sobre(1):- write('O Tamanduá-Bandeira é um mamífero nativo da América. Ele recebe esse nome uma vez que sua cauda tem forma de uma bandeira.'),nl,
+            write('São animais solitários quando atingem a idade adulta. Não são ágeis, nem agressivos.'),nl,
+            write('Alimentam-se principalmente de pequenos insetos, por exemplo, formigas, cupins, larvas, centopeias, vermes.'),nl,
+            write('Podem andar  e nadar'),nl.
+
+sobre(2):- write('Tucano é uma ave linda, muito colorida e o que chama mais atenção é o bico enorme.'),nl,
+            write(' O tucano tem cores muito vivas que chamam a atenção das fêmeas. Mas, você acha que o bico dele é pesado? '),nl,
+            write('O tucano é bom de bico... Ele gosta de muitas coisas, podem ser frutinhas e sementes, insetos, ovos de outras aves, lagartixas e pererecas. '),nl,
+            write('Podem voar'),nl.
+
+
+sobre(3):- write('O lobo-guará é um mamífero que está ameaçado em extinção.'),nl,
+            write('Diferentemente de outras espécies de lobo que vivem em matilha, o lobo-guará é um animal de hábito solitário'),nl,
+            write('O lobo-guará é um animal onívoro, pois consome uma grande variedade de animais e frutos. '),nl,
+            write('Ele se alimenta de pequenos mamíferos, como gambás e roedores, além de aves, lagartos, cobras e insetos.'),
+            write('Podem caminhar e correr.')
+            ,nl.
+
+
+alimento(1) :- [formigas,cupins, centopeias,vermes].
+alimento(2) :- [frutinhas,sementes, ovos,insetos].
+alimento(3) :- [gambas,roedores, aves,lagartos,cobra,insetos].
+
+carinho(1) :- sim.
+carinho(2) :- sim.
+carinho(3) :- no.
+
+passeio(1):- sim.
+passeio(2):- sim.
+passeio(3):- no.
+
+raca_seler :- X is random(3) + 1 ,  write('Seu pet é um '),raca(X),nl,write('Qual o nome do seu pet '),nl,read(N),Nome is N, Tipo is X, nl.
+
+clear :- write('\33\[2J').  
+
+opt(1) :- nl, write('Tamagotchi tá nascendoo...'), nl, nl, sleep(1), raca_seler,write('Parabéns! Vc acaba de ter um '), nl.
+opt(2) :-   nl, sobre(2),nl.
+opt(3) :-   nl, sobre(2),nl.
+opt(4) :-   nl, sobre(2),nl.
+opt(5) :-   nl, sobre(2),nl.
+opt(6) :-   nl, sobre(2),nl.
+opt(0) :- nl, write('Tamagotchi se foi...'), nl, nl,halt.
 
 run :- read(X), opt(X).
 
@@ -31,10 +79,19 @@ menu :-
     write('| |_) | | | (_| |/ /| |_| | (_| (_| |'), nl,
     write('|____/|_|  \\__,_/___|\\__,_|\\___\\__,_|'), nl, nl,
 
-    write('1 - Ativar'), nl,
-    write('2 -  Sair'), nl, nl.
+    write('1 - Começar'), nl,
+    write('2 - Sobre seu pet'),nl,
+    write('3 - Carinho'),nl,
+    write('5 - Passeio'),nl,
+    write('6 - Alimentar'),nl,
+    write('0 -  Sair'), nl, nl, read(Op),nl,nl,write('você escolheu :' ),write(Op),nl,
+
+    opt(Op),nl,sleep(3), menu
+
+    .
 
 :- initialization main, halt.
 
 main :-
-    clear, menu, run.
+
+    clear, menu, run.   
