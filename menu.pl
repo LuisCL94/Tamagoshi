01f33c5004b92b1d45eccdf6ecf8b5158a5711cb
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-05-17 09:19:31 -0300

    Update menu.pl

--- menu.pl
+++ menu.pl
@@ -12,18 +12,18 @@ nb_setval(nome, '').
 nb_setval(tipo, 0).
 %nb_getval(get,C).
 
-sobre(1):- write('O Tamanduá-Bandeira é um mamífero nativo da América. Ele recebe esse nome uma vez que sua cauda tem forma de uma bandeira.'),nl,
+sobre(1):- write('Tipo: 1. O Tamanduá-Bandeira é um mamífero nativo da América. Ele recebe esse nome uma vez que sua cauda tem forma de uma bandeira.'),nl,
             write('São animais solitários quando atingem a idade adulta. Não são ágeis, nem agressivos.'),nl,
             write('Alimentam-se principalmente de pequenos insetos, por exemplo, formigas, cupins, larvas, centopeias, vermes.'),nl,
             write('Podem andar  e nadar'),nl.
 
-sobre(2):- write('Tucano é uma ave linda, muito colorida e o que chama mais atenção é o bico enorme.'),nl,
+sobre(2):- write('Tipo: 2. Tucano é uma ave linda, muito colorida e o que chama mais atenção é o bico enorme.'),nl,
             write('O tucano tem cores muito vivas que chamam a atenção das fêmeas. Mas, você acha que o bico dele é pesado? '),nl,
             write('O tucano é bom de bico... Ele se alimenta de muitas coisas, podem ser frutinhas e sementes, insetos, ovos de outras aves, lagartixas e pererecas. '),nl,
             write('Podem voar'),nl.
 
 
-sobre(3):- write('O lobo-guará é um mamífero que está ameaçado em extinção.'), nl,
+sobre(3):- write('Tipo: 3. O lobo-guará é um mamífero que está ameaçado em extinção.'), nl,
             write('Diferentemente de outras espécies de lobo que vivem em matilha, o lobo-guará é um animal de hábito solitário'),nl,
             write('O lobo-guará é um animal onívoro, pois consome uma grande variedade de animais e frutos. '),nl,
             write('Ele se alimenta de pequenos mamíferos, como gambás e roedores, além de aves, lagartos, cobras e insetos.'),
@@ -41,47 +41,53 @@ alimento(2,frutinhas).
 alimento(2,ovos).
 alimento(2,insetos).
 
-alimento(3,gambas).
-alimento(3,roedores).
-alimento(3,aves).
-alimento(3,lagartos).
+alimento(3,gamba).
+alimento(3,roedore).
+alimento(3,ave).
+alimento(3,lagarto).
 alimento(3,cobra).
-alimento(3,insetos).
+alimento(3,inseto).
 
 alimentar(Alimento, X,Lista):- findall(Alimento, alimentos(X,Alimento),Lista).
 
 carinho(1,sim).
 carinho(2,sim).
-carinho(3, no).
+carinho(3, nao).
 
 passeio(1,sim).
-passeio(2,sim).
-passeio(3,no).
-passear(Pode, X, Lista):- findall(Pode, passeio(X,Pode),Lista).
+passeio(2,nao).
+passeio(3,nao).
 
-sentimento(1) :- nl, write('To feliz mas to com fome!'), nl, nl.
-sentimento(2) :- nl, write('To feliz mas to morrendo de fome!'), nl, nl.
-sentimento(3) :- nl, write('To muito feliz'), nl, nl.
-sentimento(4) :- nl, write('Estou inconsolável! :('), nl, nl.
-sentimento(5) :- nl, write('Estou muito triste e arrependido! :('), nl, nl.
+
+bronca(1,nao).
+bronca(2,sim).
+bronca(3,nao).
+
+ignora(1,nao).
+ignora(2,sim).
+ignora(3,nao).
+
+sentimento(1,A) :-carinho(A,sim),nl, write('To feliz mas to com fome!'),nl,nl; write('não é bom tentar fazer carinho'), nl, nl.
+sentimento(2,A) :-passeio(A,sim), write('To feliz mas to morrendo de fome!'),nl,nl;  write('ele não gostaria de sair '),nl,nl.
+sentimento(3,A) :-nl,write('Qual o alimento'),nl,read(B),alimento(A,B), write('To muito feliz'), nl, nl;write('não é recomendado alimentar com isso'),nl,nl.
+sentimento(4,A) :-nl,ignora(A,sim), write('Estou inconsolável! :('),nl,nl; write('você não gostaria de receber uma também, né'),nl,nl.
+sentimento(5,A) :-nl,bronca(A,sim),  write('Estou muito triste e arrependido! :('),nl,nl; write('ele pode reagir de forma ríspida'),nl,nl.
+sentimento(0,A) :-write('Tamagotchi se foi...'), nl, nl,halt.
+
 
 menuInteracao :-  nl, write(' 1 - Carinhar'), nl,
  		      write(' 2 - Passear'), nl,
 		      write(' 3 - Alimentar'), nl,
  		      write(' 4 - Ignorar'), nl,
-		      write(' 5 - Dar bronca'), nl.
+		      write(' 5 - Dar bronca'), nl,
+              write(' 0 - Sair'), nl.
 
-interagir(X, Y) :- write('Seu pet se chama '), write(X), write('. Interaja com ele...'), nl, menuInteracao, nl, read(I), sentimento(I), V=X, resposta(Y, V).
+interagir(X, Y) :- write('Seu pet se chama '), write(X), write('. Interaja com ele...'), nl,write('Seu tipo: '),nl,nl,read(A), menuInteracao, nl, read(I), sentimento(I,A), V=X, resposta(Y, V).
 
-resposta(Y, V) :-  read(I), shell(clear), write('Seu pet é um '), raca(Y), nl, sobre(Y), nl,write('---'),nl,write('---'),nl,nl, write('Seu pet se chama '), write(V), write('. Interaja com ele...'), nl, menuInteracao, sentimento(I), resposta(Y, V).
+resposta(Y, V) :-  read(I), shell(clear), write('Seu pet é um '), raca(Y), nl, sobre(Y), nl,write('---'),nl,write('---'),nl,nl, write('Seu pet se chama '), write(V), write('. Interaja com ele...'), nl, menuInteracao, sentimento(I,A), resposta(Y, V).
 
 opt(1) :- nl, write('Tamagotchi tá nascendoo...'), nl, nl, sleep(2), X is random(3) + 1 ,  shell(clear), write('Seu pet é um '), raca(X), nl, sobre(X), nl,write('Dê um nome ao seu pet... '),nl,read(N), nl, interagir(N, X), resposta, nb_setval(nome, N), nb_setval(tipo,especie(X)), nl.
 
-opt(2) :-   nl, sobre(2),nl.
-opt(3) :-   nl, sobre(2),nl.
-opt(4) :-   nl, sobre(2),nl.
-opt(5) :-   nl, sobre(2),nl.
-opt(6) :-   nl, sobre(2),nl.
 opt(0) :- nl, write('Tamagotchi se foi...'), nl, nl,halt.
 
 run :- read(X), opt(X).
